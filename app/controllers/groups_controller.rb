e024d9d5d3002856c2cb43d706f75a0a20321fbf
Miriam Ciurea <miriamciurea777@icloud.com>
2023-12-21 14:08:45 +0000

    from not working, but because there is no time...I will work on it later

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,6 +6,14 @@ class GroupsController < ApplicationController
   end
 
   def create
+    raise
+    @group = Group.find(params[:group_id])
+    @member = Member.new
+    @card.brand = @brand
+    @card.user = current_user
+    @card.save
+    # redirect_to brands_path, notice: "card created successfully!"
+    head :ok
   end
 
   def show
@@ -18,6 +26,12 @@ class GroupsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:id, :name, :max_members, user_ids: [])
   end
 
 end
